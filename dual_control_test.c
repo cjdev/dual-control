9404cc1a4bd408a10f6c185b7791da37ac8be128
Greg Wiley <gwiley@cj.com>
2017-03-28 15:34:07 -0700

    adds auth handler

--- dual_control_test.c
+++ dual_control_test.c
@@ -13,12 +13,33 @@ int pam_sm_acct_mgmt_returns_successs() {
     return result == PAM_SUCCESS;
 }
 
+int pam_sm_authenticate_returns_success() {
+    //given
+
+    //when
+    int result = pam_sm_authenticate(NULL, 0, 0, NULL);
+
+    //then
+    return result == PAM_SUCCESS;
+}
+
 int main(int argc, char* argv[]) {
-    if (pam_sm_acct_mgmt_returns_successs()) {
-        printf("success!");
+    int test1_result = pam_sm_acct_mgmt_returns_successs();
+    if (!test1_result) {
+        printf("acct management failed\n");
+    }
+
+    int test2_result = pam_sm_authenticate_returns_success();
+    if (!test2_result) {
+        printf("acct auth failed\n");
+    }
+
+    if (test1_result && test2_result) {
+        printf("success!\n");
     } else {
-        printf("fail");
+        printf("fail\n");
     }
     return 0;
 }
+
 
