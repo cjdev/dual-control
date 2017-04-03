f419d88c1ca09d834d2616e156020698fda2b7d1
Greg Wiley <gwiley@cj.com>
2017-04-03 15:42:04 -0700

    CJPM-5222: adds actual logging

--- logging.c
+++ logging.c
@@ -1,5 +1,18 @@
 #include "logging.h"
+#include <syslog.h>
+
+/*
+void openlog(const char *ident, int option, int facility);
+void syslog(int priority, const char *format, ...);
+void closelog(void);
+void vsyslog(int priority, const char *format, va_list ap);
+*/
+
+static const char program_name[] = "pam_dual_control";
 
 void log_dual_control() {
-    // do nothing
+    openlog(program_name, 0, LOG_AUTH);
+    syslog(LOG_INFO, "Logged: ");
+    closelog();
 }
+
