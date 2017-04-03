538cecaea66e0aeb507e5957a3afaeda73627b70
Greg Wiley <gwiley@cj.com>
2017-04-03 15:55:10 -0700

    CJPM-5222 change facility

--- logging.c
+++ logging.c
@@ -11,8 +11,8 @@ void vsyslog(int priority, const char *format, va_list ap);
 static const char program_name[] = "pam_dual_control";
 
 void log_dual_control() {
-    openlog(program_name, 0, LOG_AUTH);
-    syslog(LOG_INFO, "Logged: ");
+    openlog(program_name, 0, LOG_AUTHPRIV);
+    syslog(LOG_NOTICE, "Logged: ");
     closelog();
 }
 
