3d1dbe5970ba0393df2b61d528eadd69c1e73347
Greg Wiley <gwiley@cj.com>
2017-04-07 15:09:49 -0700

    memory management improvements

--- token.c
+++ token.c
@@ -39,85 +39,42 @@ size_t buffer_size(buffer_t buffer) {
 }
 
 
-int user_is_known(const char *user, buffer_t buffer) {
-    struct passwd *passwd = 0;
-    passwd = (struct passwd *) malloc(sizeof(struct passwd));
+int get_passwd(const char *user, struct passwd *passwd, buffer_t buffer) {
     struct passwd *found_passwd = 0;
     getpwnam_r(user, passwd, use_buffer(buffer), buffer_size(buffer), &found_passwd);
-    int known = found_passwd != 0;
-
-    free(passwd);
-
-    return known;
-
-}
-
-char *get_home_directory(const char *user, buffer_t buffer) {
-    struct passwd *passwd = 0;
-    passwd = (struct passwd *) malloc(sizeof(struct passwd));
-    struct passwd *found_passwd = 0;
-    getpwnam_r(user, passwd, use_buffer(buffer), buffer_size(buffer), &found_passwd);
-
-    free(passwd);
-
-    if (found_passwd) {
-        return found_passwd->pw_dir;
-    }
-
-    return 0;
+    return (found_passwd != 0);
 }
 
 int validate_token(const char *token) {
 
-    char *user = 0;
-    char *dup_token = 0;
-    struct stat *file_stat = 0;
     char *filepath = 0;
-
+    char *working_token = 0;
+    buffer_t buffer = allocate_buffer();
 
     int ok = 0;
 
-
-    // duplicate
     int token_length = strlen(token);
-    user = (char *) malloc((token_length + 1) * sizeof(char));
-    strcpy(user, token);
-
-    // find the first colon
-    char *colon = strchr(user, ':');
+    working_token = (char *) malloc((token_length + 1) * sizeof(char));
+    strcpy(working_token, token);
+    char *colon = strchr(working_token, ':');
     if (!colon) {
         goto finally;
     }
 
-    // poke a zero so dup is the username
     *colon = 0;
+    char *user = working_token;
+    char *user_token = colon + 1;
 
-    dup_token = (char *)malloc((token_length + 1) * sizeof(char));
-    strcpy(dup_token, token);
-    char *token_colon = strchr(dup_token, ':');
-    if (!token_colon) {
-       goto finally;
-    }
-
-     char *user_token = token_colon + 1;
-
+    struct passwd passwd;
+    int user_found = get_passwd(user, &passwd, buffer);
 
     // check if user is known
-    buffer_t user_buffer = allocate_buffer();
-    if(!user_is_known(user, user_buffer)) {
+    if(!user_found) {
        goto finally;
     }
 
-    // find the token for found user
-    buffer_t directory_buffer = allocate_buffer();
-    char *directory = get_home_directory(user, directory_buffer);
-    if (!directory) {
-        goto finally;
-    }
+    const char *directory = passwd.pw_dir;
 
-    // stat
-
-    file_stat = (struct stat *) malloc(sizeof(struct stat));
     int dir_len = strlen(directory);
     int fname_len = strlen(".dual_control");
     filepath = (char *)malloc((dir_len + 1 + fname_len + 1) * sizeof(char));
@@ -126,11 +83,11 @@ int validate_token(const char *token) {
     strcat(filepath, "/");
     strcat(filepath, ".dual_control");
 
-    int check_file = stat(filepath, file_stat);
+    struct stat file_stat;
+    int check_file = stat(filepath, &file_stat);
     if (check_file) {
         goto finally;
     }
-
 
     // read the file and grab token
     char fetched_token[7];
@@ -148,11 +105,10 @@ int validate_token(const char *token) {
 
     finally:
 
-    free(user);
-    free_buffer(&user_buffer);
-    free_buffer(&directory_buffer);
-    free(file_stat);
     free(filepath);
+    free(working_token);
+    free_buffer(&buffer);
+
     return ok;
 }
 
