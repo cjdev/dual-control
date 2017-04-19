d91ed0ed3762d9ba29f6fe7e7aaa1fa55e3f72f7
Greg Wiley <gwiley@cj.com>
2017-04-19 10:57:40 -0700

    remove old conversation class

--- conversation.h
+++ conversation.h
@@ -51,30 +51,5 @@ public:
     }
 };
 
-class token_conversation
-{
-public:
-    virtual ~token_conversation() {}
-    virtual std::string token()
-    {
-        return "";
-    }
-    virtual std::string user_name()
-    {
-        return "";
-    }
-};
-
-class pam_token_conversation : public token_conversation
-{
-public:
-    std::string user_;
-    std::string token_;
-    pam_token_conversation (pam_handle_t *pamh);
-    pam_token_conversation (pam_handle_t *pamh, const pam_p);
-    std::string token();
-    std::string user_name();
-};
-
 #endif
 
