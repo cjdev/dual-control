175429b4b0fad0c1773ef9adae4a5b11b84f91d8
Greg Wiley <aztec.rex@jammm.com>
2017-04-20 22:37:21 -0700

    format

--- conversation.cc
+++ conversation.cc
@@ -1,3 +1,13 @@
+/* Copyright (C) CJ Affiliate
+ *
+ * You may use, distribute and modify this code under  the
+ * terms of the GNU Public License.
+ *
+ * You should have received a copy of the license with this
+ * file. If not, you will find a copy in the "LICENSE" file
+ * at https://github.com/cjdev/dual-control.
+ */
+
 #include <security/pam_modules.h>
 #include <algorithm>
 #include <vector>
