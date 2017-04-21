210c0dc8c08b80b2de2f2eb45e99003fe55d495b
Greg Wiley <aztec.rex@jammm.com>
2017-04-20 22:37:04 -0700

    improve formatter

--- format.sh
+++ format.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-for f in *.h *.cc; do echo "" >> $f; { rm $f;cat -s >$f; }<$f; done
-astyle -q --options=$(dirname $0)/astylerc *.h *.cc
+
+for f in "${@}"; do
+  astyle -q --options=$(dirname $0)/astylerc $f
+  echo "" >> $f
+  if ! grep -q 'Copyright.*CJ' $f; then
+      cat << EOF > $f.temp_
+/* Copyright (C) CJ Affiliate
+ *
+ * You may use, distribute and modify this code under  the
+ * terms of the GNU Public License.
+ *
+ * You should have received a copy of the license with this
+ * file. If not, you will find a copy in the "LICENSE" file
+ * at https://github.com/cjdev/dual-control.
+ */
+
+EOF
+  fi
+  cat -s $f >> $f.temp_
+  mv $f.temp_ $f
+done
 
