7684972aa511a964724fad36bb4c5c9449a2a6dd
Greg Wiley <aztec.rex@jammm.com>
2017-04-20 22:57:16 -0700

    format

--- dual_control.h
+++ dual_control.h
@@ -1,3 +1,14 @@
+/* Copyright (C) CJ Affiliate
+ *
+ * You may use, distribute and modify this code under  the
+ * terms of the  GNU General Public License  version 2  or
+ * later.
+ *
+ * You should have received a copy of the license with this
+ * file. If not, you will find a copy in the "LICENSE" file
+ * at https://github.com/cjdev/dual-control.
+ */
+
 /* PAM handlers class interface. The methods on the dual control class
  * correspond directly to the specified PAM handler entrypoints.
  *
