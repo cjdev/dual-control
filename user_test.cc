ccfeb62d1e5a14f73a865569890afe593061c62a
Greg Wiley <aztec.rex@jammm.com>
2017-04-20 22:54:31 -0700

    revert "format"
    This reverts commit 175429b4b0fad0c1773ef9adae4a5b11b84f91d8.

--- user_test.cc
+++ user_test.cc
@@ -1,13 +1,3 @@
-/* Copyright (C) CJ Affiliate
- *
- * You may use, distribute and modify this code under  the
- * terms of the GNU Public License.
- *
- * You should have received a copy of the license with this
- * file. If not, you will find a copy in the "LICENSE" file
- * at https://github.com/cjdev/dual-control.
- */
-
 #include <memory>
 #include <iostream>
 #include "user.h"
